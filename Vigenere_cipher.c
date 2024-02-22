e9161fa761536d6c0b3781a7cde04cfabac8ac36
ilyarybka <113148796+ilyarybka@users.noreply.github.com>
2024-02-21 22:57:56 -0500

    Update Vigenere_cipher.c

--- Vigenere_cipher.c
+++ Vigenere_cipher.c
@@ -1,27 +1,3 @@
-/*=============================================================================
-| Assignment: pa01 - Encrypting a plaintext file using the Vigenere cipher
-|
-| Author: Iliya Klishin
-| Language: c, c++, Java
-|
-| To Compile: javac pa01.java
-| gcc -o pa01 pa01.c
-| g++ -o pa01 pa01.cpp
-|
-| To Execute: java -> java pa01 kX.txt pX.txt
-|             or c++ -> ./pa01 kX.txt pX.txt
-|             or c -> ./pa01 kX.txt pX.txt
-| where kX.txt is the keytext file
-| and pX.txt is plaintext file
-|
-| Note: All input files are simple 8 bit ASCII input
-|
-| Class: CIS3360 - Security in Computing - Spring 2022
-| Instructor: McAlpin
-| Due Date: February 27
-|
-+=============================================================================*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -193,11 +169,3 @@ int main(int argc, char** argv) {
   //fclose(w);
   return 0;
 }
-/*=============================================================================
-| I Iliya Klishin (il351709) affirm that this program is
-| entirely my own work and that I have neither developed my code together with
-| any another person, nor copied any code from any other person, nor permitted
-| my code to be copied or otherwise used by any other person, nor have I
-| copied, modified, or otherwise used programs created by others. I acknowledge
-| that any violation of the above terms will be treated as academic dishonesty.
-+=============================================================================*/
